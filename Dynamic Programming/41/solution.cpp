3c3445156f2f43f014f3d565fc05d41cc5533ee6
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-03 22:51:58 +0100

    Comment of the solution

--- Dynamic Programming/41/solution.cpp
+++ Dynamic Programming/41/solution.cpp
@@ -12,11 +12,26 @@ using namespace std;
 #include "Matriz.h"
 
 /*@ <answer>
-  
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
- 
+
+ Se quiere conocer la columna por la que se tiene que empezar en un tablero 
+ para conseguir la mayor suma al recorrer dicho tablero de forma diagonal o vertical.
+
+ Se utilizará la programación dinámica para resolver el problema atendiendo a la función recursiva.
+   - tablero(i, j) = máxima suma que se obtiene al tener un tablero de i y j.
+
+ Como se tiene una función recursiva, se parten de casos básicos los cuales son:
+   - tablero(i, 0) = 0 si no se tienen columnas.
+   - tablero(0, j) = 0 si no se tienen filas.
+ Por contrapartida se tiene el caso recursivo:
+   - tablero(i, j) += max(tablero[i - 1][j], max(tablero[i - 1][j - 1], tablero[i - 1, j + 1]))
+ Se recorre la fila N (siendo N el número de filas) para conocer el elemento más grande y su respectivo índice
+
+ Se utiliza una matriz sin posiblidad de utilizar un vector para mejorar el coste del espacio.
+ La matriz tendrá como dimensión (N + 1) * (N + 2) para no tener problemas al realizar la función recursiva.
+
+ Así pues se tienen los costes:
+   - En función del tiempo: O(N^2) siendo N el número de elementos que se tiene en el tablero.
+   - En función del espacio: O(N^2).
  @ </answer> */
 
 
