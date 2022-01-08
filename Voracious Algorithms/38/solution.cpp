18cd4acb0ed8ce19e2f2c4e4989b59bde0528ee6
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-08 13:56:23 +0100

    Added comment to the exercise

--- Voracious Algorithms/38/solution.cpp
+++ Voracious Algorithms/38/solution.cpp
@@ -12,8 +12,33 @@ using namespace std;
 
 /*@ <answer>
 
+ Supongamos las personas ordenadas dependiendo de sus pesos de forma que P[0] <= P[1] <= ...
 
+ El algoritmo voraz V toma la el peso más pequeño con el más grande disponible que pueden ocupar 
+ un telesilla sin superar el peso máximo indicado. En el caso de que no se pueda dar esto, una sola persona 
+ ocupará una sola telesila. Por tanto, el algoritmo voraz es una secuencia de conjuntos de personas que componen
+ un telesilla
+
+ Ejemplo:
+    P 10 10 10 10
+
+ La solución voraz: {1, 4} {2, 3} siendo estos los índices
+
+ Sea O una solución óptima y sea i la primera posición donde O y V difieren O[i] != V[i]
+ Entonces el conjunto de personas que conforman el telesilla en V[i] ha sido asignada más adelante en O.
+ Sea k la posición que cumple que V[i] == O[k]
+
+ P:  P[0] <= P[1] ...
+ V:  V[0] V[1] ... | V[i]
+        =   =  ... |  != 
+ O:  O[0] O[1] ... | O[i] ... O[k] == V[k]
+
+ Caso 1: V[i] == O[i] (los pesos conformados por el telesilla en la posición i son los mismos tanto en la voraz como en la óptima)
+
+ Caos 2: V[i] != O[i] (al menos un peso en la solución voraz no coincide con la solución óptima). Si esto sucede, se intercambiaría i y k
+ y el número de telesillas empleado para la resolución del caso de prueba no cambia. 
  
+ El coste del algoritmo voraz es O(NlogN) siendo N el número de pesos de cada caso de prueba
  @ </answer> */
 
 
@@ -37,7 +62,7 @@ bool resuelveCaso() {
     for (int i = 0; i < N; i++)
         cin >> pesos[i];
     sort(pesos.begin(), pesos.end(), less<int>());
-    
+
     vector<Telesilla> telesillas;
     int menor = 0;
     int mayor = N - 1;
