af1a4e31b487bd31c9f0c8934b2d4956083ad0a3
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-10 17:41:02 +0100

    Exercise 14 corrected by judge

--- Graphs/NonDirected/14/solution.cpp
+++ Graphs/NonDirected/14/solution.cpp
@@ -4,17 +4,22 @@
  *
  *@ </answer> */
 
-#include "Grafo.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
+#include "Grafo.h"
 using namespace std;
 
 /*@ <answer>
   
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
+ Se pide conocer el tamaño de la componente conexa en el que se encuentra cada vertice.
+ Para ello, se realizará un recorrido en profundidad que almacenará en un vector (componente)
+ el componente al que pertenece cada vertice. Además se tendrá un vector que contendrá para cada
+ componente su respectivo tamaño.
+
+ El tamaño del ejercicio es de O(V + A) siendo V el número de vertices que componen el grafo y A el número de
+ aristas.
  
  @ </answer> */
 
@@ -24,68 +29,65 @@ using namespace std;
 // ================================================================
 //@ <answer>
 
-
-class Componentes {
+class ComponenteConexa {
 public:
-   Componentes(Grafo const& G) : visit(G.V(), false), componente(G.V()) {
-      for (auto v = 0; v < G.V(); v++) {
-         if (!visit[v]) {
-            int tam = dfs(G, v);
-            tamComp.push_back(tam);
-         }
-      }
-   }
+    ComponenteConexa(Grafo const& g) : visit(g.V(), false), componente(g.V()) {
+        
+        for (int v = 0; v < g.V(); v++) {
+            if (!visit[v]) {
+                int tam = dfs(g, v);
+                tamComp.push_back(tam);
+            }
+        }
+    }
 
-   int tamCompV(int v) {
-      return tamComp[componente[v]];
-   } 
+    int getTamV(int v) const { return tamComp[componente[v]]; }
 
 private:
-   vector<bool> visit;
-   vector<int> componente;
-   vector<int> tamComp;
+    vector<bool> visit;
+    vector<int> tamComp;
+    vector<int> componente;
 
-   int dfs(Grafo const& G, int v) {
-      visit[v] = true;
-      componente[v] = tamComp.size();
-      int tam = 1;
-      for (int w : G.ady(v)) {
-         if (!visit[w])
-            tam += dfs(G, w);
-      }
+    int dfs(Grafo const& g, int v) {
+        visit[v] = true;
+        componente[v] = tamComp.size();
+        int tam = 1;
+        for (int w : g.ady(v)) {
+            if (!visit[w]) {
+                tam += dfs(g, w);
+            }
+        }
 
-      return tam;
-   }
+        return tam;
+    }
 };
 
 bool resuelveCaso() {
-   
-   int N, M;
-   cin >> N >> M;
-   if (!cin) 
-      return false;
-   
-   Grafo g(N + 1);
-   for (int i = 1; i <= M; i++) {
-      int v;
-      cin >> v;
-      for (int j = 1; j <= v; j++) {
-         int w;
-         cin >> w;
-         if (v != w)
+
+    int N, M;
+    cin >> N >> M;
+    if (!cin)
+        return false;
+    
+    Grafo g(N + 1);
+    while (M--) {
+        int C, v, w;
+        cin >> C;
+        if (C > 0)
+            cin >> v;
+        for (int i = 1; i < C; i++) {
+            cin >> w;
             g.ponArista(v, w);
-      }
-   }
+            v = w;
+        }
+    } 
 
-   Componentes comp(g);
-   for (int i = 1; i < g.V(); i++)
-      cout << comp.tamCompV(i) << " ";
+    ComponenteConexa componentes(g);
+    for (int v = 1; v < g.V(); v++) 
+        cout << componentes.getTamV(v) << " ";
+    cout << endl;
 
-   cout << endl;
-   cout << g << endl;
-   // escribir la solución
-
-   return true;
+    return true;
 }
 
 //@ </answer>
